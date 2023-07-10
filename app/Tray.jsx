817960ce3a81a3da8b5959b059949debe95a1dd1
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-07-10 14:50:57 -0400

    added reset

--- app/Tray.jsx
+++ app/Tray.jsx
@@ -6,16 +6,36 @@ export default function Tray({setSelectRestaurant}) {
         const chosen = Math.random()
         setSelectRestaurant(chosen)
     }
+
+    const clear= () => {
+        setSelectRestaurant(0)
+
+
+    }
     return(
         <View style={styles.Tray}>
+            <View style={styles.buttonList}>
             <TouchableOpacity onPress ={choose} style={styles.button}>
             <Text style= {styles.buttonText}> Shuffle</Text>
             </TouchableOpacity>
+
+            <TouchableOpacity style= {styles.resetButton}>
+              <Text onPress ={clear}style= {styles.buttonText}> Reset </Text>
+            </TouchableOpacity>
+           </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    buttonList:{
+        flexDirection: 'row',
+        width: '80%',
+        justifyContent: 'space-evenly',
+        
+
+
+    },
     button:{
         backgroundColor: 'pink',
         paddingHorizontal: 24, 
@@ -24,7 +44,19 @@ const styles = StyleSheet.create({
         borderStyle: 'solid',
         borderWidth: 1,
         borderColor: 'black',
+        
     },
+
+    resetButton: {
+        backgroundColor: "grey",
+        paddingHorizontal: 24,
+        paddingVertical: 8,
+        borderRadius: 4,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'black',
+
+      },
 
     buttonText: {
         color: 'white',
@@ -40,4 +72,6 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
         
     }
+
+
 })
